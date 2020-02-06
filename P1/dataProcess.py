e6f0db225f62eef3c9cd8652fa1661602f30c92b
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2020-02-06 01:29:43 -0500

    dataProcess finished

--- P1/dataProcess.py
+++ P1/dataProcess.py
@@ -1,7 +1,5 @@
-#author: Yuhang Zhang
+#author: Yuhang Zhang & Oliva Xu 
 #This class contains functions that build and analysis dataset samples.
-#Sklearn shall be installed: https://calebshortt.com/2016/01/15/installing-scikit-learn-python-data-mining-library/
-
 #=================================imports==================================
 import math
 import csv
@@ -14,47 +12,51 @@ from collections import defaultdict
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
-#=========================== Helper Functions =============================
-
-#===================== General OneHotEncode function ======================
-def encode(data):
+#=========================== 2 Helper Functions: ==========================
+#===================== Encode X string variables into integers ============
+def encode_X(data):
     values = array(data)
+
     # integer encode
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(values)
+    return integer_encoded
+
+
+#========= Encode y result into bianry numbers **ONE HOT ENCODING** =======
+
+def encode_y(data):
+    values = array(data)
+
+    # integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+    
     # binary encode
     onehot_encoder = OneHotEncoder(sparse = False)
     integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
     onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
     return onehot_encoded
 
-    #** Not required **
-    # invert first example ** Not required **
-    #inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
-    #print(inverted)
+
 
 #===== 4 individual matrix-builder as each dataset has few in common ======
-
 #================== matrix bilder for ionosphere.txt ======================
 def ionosphere_builder():
     global iono_X, iono_y
     iono_X = genfromtxt('ionosphere.txt', dtype='float32', delimiter=',', filling_values = -99, usecols = (range(34)))
+    iono_y = genfromtxt('ionosphere.txt', dtype='S8', delimiter=',', filling_values = 'g', usecols = (34))
     print(iono_X.shape)
 
     # Remove malsamples
     for i in range(len(iono_X)):
-        for j in range(len(iono_X[i])):
-            if iono_X[i][j] == -99 :
-                iono_X = np.delete(iono_X, i, 0)
-    #print(iono_X.shape)
+        for j in range(len(iono_X[i-1])):
+            if iono_X[i-1][j] == -99 :
+                iono_X = np.delete(iono_X, i-1, 0)
 
     # Remove feature at column 1 
     iono_X = np.delete(iono_X, 1, 1)
-    iono_y = genfromtxt('ionosphere.txt', dtype='S8', delimiter=',', filling_values = 'g', usecols = (34))
     
-    #TEST1
-    #print(iono_X)[1][3]
-    #print(iono_y)
 
 #================== matrix bilder for adult.txt ===========================
 def adult_builder():
@@ -63,34 +65,71 @@ def adult_builder():
     adult_y = genfromtxt('adult.txt', dtype='S8', delimiter=',', filling_values = 'MAL_SAMPLE', usecols = (14))
     #print(adult_X.shape)
 
+    #Integer Encode of each column
+    for i in range(len(adult_X[0])):
+        if not any(char.isdigit() for char in adult_X[0][i]):
+            data = adult_X[:, i]
+            adult_X[:, i] = encode_X(data)
+
     #Delete malfeatures
     adult_X = np.delete(adult_X, [7, 8, 11, 12], 1)
 
-    #One Hot Encode of each column
-
-    for i in range(len(adult_X[0])):
-        if not adult_X[0][i].isdigit():
-            data = adult_X[:, i]
-            #print(data)
-            print(encode(data))
-
-    #adult_X = finalmatrix\
-    #print(adult_X.shape)
-
     #Delete malsamples 
     for i in range(len(adult_X)):
-        for j in range(len(adult_X[i])):
-            if adult_X[i][j] == 'MAL_SAMPLE' :
-                adult_X = np.delete(adult_X, i, 0)
-    #print(adult_X)
-    #print(adult_X.shape)
+        for j in range(len(adult_X[i-1])):
+            if adult_X[i-1][j] == 'MAL_SAMPLE' :
+                adult_X = np.delete(adult_X, i-1, 0)
 
-    
-    #print(adult_X)
-    #print(adult_X.shape)
+    #ONE HOT ENCODING of y 
+    adult_y = encode_y(adult_y)
 
 
+#================== matrix bilder for bank.txt ============================
+def bank_builder():
+    global bank_X, bank_y
+    bank_X = genfromtxt('bank.txt', dtype='S20', delimiter=';', filling_values = 'MAL_SAMPLE', usecols = np.arange(0,15))
+    bank_y = genfromtxt('bank.txt', dtype='S2', delimiter=';',  usecols = (16))
 
+    #Integer Encode of each column
+    for i in range(len(bank_X[0])):
+        if not any(char.isdigit() for char in bank_X[0][i]):
+            data = bank_X[:, i]
+            bank_X[:, i] = encode_X(data)
+
+    #ONE HOT ENCODING of y 
+    bank_y = encode_y(bank_y)
+
+    #Delete malsamples 
+    for i in range(len(bank_X)):
+        for j in range(len(bank_X[i-1])):
+            if bank_X[i-1][j] == 'MAL_SAMPLE' :
+                bank_X = np.delete(bank_X, i-1, 0)
+
+#================== matrix bilder for breast-cancer.txt =====================
+def breastCancer_builder():
+    global cancer_X, cancer_y
+    cancer_X = genfromtxt('breast-cancer.txt', dtype='S20', delimiter=',', filling_values = 'MAL_SAMPLE', usecols = np.arange(0,9))
+    cancer_y = genfromtxt('breast-cancer.txt', dtype='S8', delimiter=',',  usecols = (9))
+
+    #Integer Encode of each column
+    for i in range(len(cancer_X[0])):
+        if not any(char.isdigit() for char in cancer_X[0][i]):
+            data = cancer_X[:, i]
+            cancer_X[:, i] = encode_X(data)
+
+    #ONE HOT ENCODING of y 
+    cancer_y = encode_y(cancer_y)
+
+    #Delete malsamples 
+    for i in range(len(cancer_X)):
+        for j in range(len(cancer_X[i-1])):
+            if cancer_X[i-1][j] == 'MAL_SAMPLE' :
+                cancer_X = np.delete(cancer_X, i-1, 0)
+
+#=========================== Test funtions ================================
+#Test funtions: The underlining functions shall be used in the main function 
 
 #ionosphere_builder()
-adult_builder()
+#adult_builder()
+#bank_builder()
+#breastCancer_builder()
