97f6b518a73312a8b0e65117e63394792478ec71
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2020-02-07 01:44:43 -0500

    NaiveBayesClassifier formation

--- P1/naiveBayesClassifier.py
+++ P1/naiveBayesClassifier.py
@@ -1,10 +1,10 @@
 # Author: Yuhang Zhang 
-# Naive Bayes Classifier is feasible here since we has filter data samples in the way that features are independent
+# Naive Bayes Classifier is feasible here since we have filtered data samples in the way that features are mutually independent
 # Assumption:   1. matrix_X, and matrix_y
 #               2. For matrix_X:
 #                   continous elements have type "float" 
 #                   discrete elements are encoded as "integer"
-#               4. For matrix_y:
+#               5. For matrix_y:
 #                   binary results are coded either integer "0", or "1"    
 #=================================imports==================================
 import math
@@ -12,7 +12,9 @@ import numpy as np
 from math import sqrt
 from math import exp
 from math import pi
+
 #=========================== Helper Functions: ============================
+
 # calculate the mean of a list of numbers
 def mean(numbers):
 	return sum(numbers) / float(len(numbers))
@@ -28,13 +30,18 @@ def calculate_probability(x, mean, stdev):
 	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
 	return (1 / (sqrt(2 * pi) * stdev)) * exponent
 
+
 #=========================== naiveBayesClassifer ==========================
 def nbc(sample, matrix_X, matrix_y):
-    # get size of col and row
+    
+    # get size of col and row from matrix_X
     num_row = matrix_X.shape[0]
     num_col_X = matrix_X.shape[1]
 
+    # Step_ 1
     # calculate the prior probability
+
+    # count the number of results that equal to 0  
     count = 0
     for i in range(num_row):
         if matrix_y[i] == 0:
@@ -42,11 +49,14 @@ def nbc(sample, matrix_X, matrix_y):
     p_prior_0 = count / num_row
     p_prior_1 = 1 - p_prior_0
 
+    # Step_2
     # calculate the likelihood
-    p_discrete_0, p_discrete_1, p_continous_0, p_continous_1 = 1, 1, 1, 1
+
+    #likelihood variables shall be defined as float numbers 
+    p_discrete_0, p_discrete_1, p_continous_0, p_continous_1 = 1., 1., 1., 1.
 
     for i in range(num_col_X):
-        # categorical distribution
+        # discrete numbers ** categorical distribution **
         if isinstance(num_col_X[0][i], int):
             count_d_0, count_d_1 = 0, 0
             for j in range(num_row):
@@ -57,7 +67,9 @@ def nbc(sample, matrix_X, matrix_y):
             # accumulate p 
             p_discrete_0 = p_discrete_0 * (count_d_0/num_row)    
             p_discrete_1 = p_discrete_1 * (count_d_1/num_row)         
-        else: # continous numbers 
+        # continous numbers ** gaussian distribution **
+        else: 
+            #load features according to result number
             nums_0, nums_1 = [], []
             for j in range(num_row):
                 if matrix_y[j] == 0:
@@ -75,7 +87,10 @@ def nbc(sample, matrix_X, matrix_y):
     p_0 = p_prior_0 * p_discrete_0 * p_continous_0
     p_1 = p_prior_1 * p_discrete_1 * p_continous_1
 
+    # Output
     if p_0 > p_1:
-        print(0)     
+        print(0)
+        return 0     
     else:
-        print(1)    
+        print(1)
+        return 1    
