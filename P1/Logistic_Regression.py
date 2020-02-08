ecf05a40fde8f28feb004cb9a947ac2ca7ce3f49
serevic <37199202+serevic@users.noreply.github.com>
2020-02-08 16:25:26 -0500

    Update Logistic_Regression.py

--- P1/Logistic_Regression.py
+++ P1/Logistic_Regression.py
@@ -2,14 +2,15 @@
 import numpy as np
 import math
 from numpy.linalg import inv
+from dataProcess import ionosphere_builder
 
 
 class Logistic_Regression:
 
 	# Data Matrix X
-	Xdata = []
+	Xdata = [[]]
 	# Target Matrix Y
-	Ytarget = []
+	Ytarget = [[]]
 	# Member Viariables for evalation
 	tp = 0
 	tn = 0
@@ -17,44 +18,41 @@ class Logistic_Regression:
 	fn = 0
 
 
+	lrate = 0.1
+	inum = 500
+	eps = 0.01
 
 	# MAY BE USEFUL
-	# Yhat = sigma(logit)
-	Yhat = [[]]
-	# binary prediction {0,1}
-	prediction = [[]]
+	Yhat = []
 
 
 	def __init__(self, dataname):
-    	
-    		self.dataname = dataname
-    		# Load the datasets into NumPy objects
-    		loadData(self.Xdata, self.Ytarget)
-    	
-    		# TODO: Randomize the datasets
-		#
-		#
-
-
-	def fit(X, Y, learningRate, iterNum, eps):
 		
-		### Direct Solution: w*=((X.T)(X))^(-1)(X.T)(Y)
+		self.dataname = dataname
+		# Load the datasets into NumPy objects
+		self.Xdata,self.Ytarget = ionosphere_builder()
 		# Add Bias (concatenate a 1 to x)
-		Xb = np.insert(X, 0, 1, axis=1)
+		self.Xdata = np.insert(self.Xdata, 0, 1, axis=1)
+		# TODO: Randomize the datasets
+
+
+	def fit(self, X, Y, learningRate, iterNum, epsilon):
+
+		### Direct Solution: w*=((X.T)(X))^(-1)(X.T)(Y)
 		# Compute X.T dot X
-		product1 = np.dot(Xb.T, Xb)
+		product1 = np.dot(X.T, X)
 		# Compute X.T dot Y
-		product2 = np.dot(Xb.T, Y)
+		product2 = np.dot(X.T, Y)
 		# Compute w*
 		wstar = inv(product1).dot(product2)
 
 		### Call gradientDescent
-		self.gradientDescent()
+		self.gradientDescent(X, Y, learningRate, iterNum, epsilon)
 
 		return wstar;
 
 
-	def gradientDescent(X, Y, learningRate, eps, iterNum=1000):
+	def gradientDescent(self, X, Y, learningRate, iterNum, epsilon):
 		
 		### Gradient-Descent
 		# Refrence: Lecture 6 "GD for logistic Regresssion"
@@ -63,15 +61,15 @@ class Logistic_Regression:
 		g = np.inf
 		count = 0
 		
-		while (np.linalg.norm(g)>eps) && (count<iterNum):
-			g = gradient(X, Y, w)
+		while (np.linalg.norm(g)>epsilon) and (count<iterNum):
+			g = self.gradient(X, Y, w)
 			w = w - learningRate*g
-			count++
+			count = count + 1
 
 		return w
 
 	
-	def stochastic_gradientDescent(X, Y, learningRate, eps, iterNum=1000)
+	def stochastic_gradientDescent(X, Y, learningRate, iterNum, epsilon):
 		
 		### EXTRA FEATURE: Stochastic Gradient-Descent
 		# Refrence: Lecture 6 "SGD for logistic Regresssion"
@@ -80,72 +78,113 @@ class Logistic_Regression:
 		g = np.inf
 		count = 0
 		
-		while (np.linalg.norm(g)>eps) && (count<iterNum):
+		while (np.linalg.norm(g)>epsilon) and (count<iterNum):
 			n = np.random.randint(N)
 			g = gradient(X[[n],:], Y[[n]], w)
 			w = w - learningRate*g
-			count++
+			count = count + 1
 
 		return w
 	
 
-	def gradient(X, Y, w):
+	def gradient(self, X, Y, w):
 
 		### Helper Method to Compute Gradient
 		# Refrence: Lecture 6 "GD for logistic Regresssion"
 		N,D = X.shape
-		yh = np.dot(X, W)
-		grad = np.dot(X.T, yh-y) / N
+		yh = np.dot(X, w)
+		grad = np.dot(X.T, yh-Y) / N
 		return grad
+
 
 	def truthTable(Yhat, Y):
 		
 		### Helper Method to Evaluate Accuracy
 		### EXTRA FEATURE: Positive & Negative
 		for index in range(len(Yhat)):
-			if (Yhat[index][0]>=0.5) && (Y[index][0]==1):
-				tp++;
-			if (Yhat[index][0]<0.5) && (Y[index][0]==0):
-				tn++;
-			if (Yhat[index][0]>=0.5) && (Y[index][0]==0):
-				fp++;
-			if (Yhat[index][0]<0.5) && (Y[index][0]==1):
-				fn++;
+			if (Yhat[index]>=0.5) and (Y[index]==1):
+				tp = tp + 1;
+			if (Yhat[index]<0.5) and (Y[index]==0):
+				tn = tn + 1;
+			if (Yhat[index]>=0.5) and (Y[index]==0):
+				fp = fp + 1;
+			if (Yhat[index]<0.5) and (Y[index]==1):
+				fn = fn + 1;
 
 
-	def predict(self, Xfortest):
+	def predict(self):
 
 		### Do prediction on TEST sets
 		# Yhat = sigma((wstar.T)(X))
 
 		# TODO: select sets for training 
-		#
-		#
-
-		# Compute Yhat
-		wstar = self.fit(Xfortrain, Yfortrain, learningRate, iterNum, eps)
+		Xfortrain, Yfortrain = self.selectTrainSets()
+		Xfortest = self.selectTestSets()
+		wstar = self.fit(Xfortrain, Yfortrain, self.lrate, self.inum, self.eps)
 		designMatrix = np.dot(Xfortest, wstar)
+		
+		#print(designMatrix)
+		
 		for index in range(len(designMatrix)):
-			logit = designMatrix[index][0]
-			Yhat[index][0] = self.logistic_function(logit)
-			# Convert to binary by thresholding 0.5
-			if Yhat[index][0]>=0.5:
-				prediction[index][0] = 1;
-			else:
-				prediction[index][0] = 0;
-			# print prediction[index][0]		
+			logit = designMatrix[index]
+			self.Yhat.append(self.logistic_function(logit))
+			# print yhat[index][0]
+		print(self.Yhat)
+		print(self.Ytarget[310:320])
 
 
-	def logistic_function(logit):
+	def selectTrainSets(self):
+		#print(self.Xdata[0:300, :])
+		#print(self.Ytarget[0:300])
+		return self.Xdata[0:300, :], self.Ytarget[0:300]
+
+
+	def selectTestSets(self):
+		return self.Xdata[310:320, :]
+
+
+	def logistic_function(self, logit):
 		return 1.0 / (1.0 + math.e**(-logit))
 
 
-	def kCrossValidation():
+	def kfcValidation(data_X,data_y,fold):
+		total_rows = np.size(data_X,0)
+		total_accuracy = 0
+		for i in range(1,fold):
+
+			# prepare training data and validation data for  k fold
+
+			vali_first_row = int(((i-1)/fold) * total_rows)
+			vali_last_row = int((i/fold) * total_rows)
+			vali_X = data_X[num_first_row,:]
+			vali_y = data_y[num_first_row,:]
+
+			for j in range(vali_first_row,vali_last_row):
+				vali_X = np.row_stack((vali_X,data_X[j,:]))
+				vali_y = np.row_stack((vali_y,data_y[j,:]))
+
+				train_X =  data_X[0,:]
+				train_y =  data_Y[0,:]
+
+				if i == 1 :
+					train_X = data_X[vali_last_row+1:,]
+					train_y = data_y[vali_last_row+1:,]
+				elif i == k :
+					train_X  = data_X[0:vali_first_row,:]
+					train_y  = data_y[0:vali_first_row,:]
+				else :
+					train_X = np.row_stack(( data_X[0:vali_first_row,:],data_X[vali_last_row+1:,:] ))
+					train_y = np.row_stack(( data_y[0:vali_first_row,:],data_y[vali_last_row+1:,:] ))
+
+					train_X = np.delete(train_X,0,0)
+					train_y = np.delete(train_X,0,0)
+
+					# use train xy, vali xy to get accuracy
+					# accuracy = ### call Function with train X ,train Y ,val_X and Val_y
+					# total_accuracy = total_accuracy + accuracy
+
+		return (total_accuracy/fold)
+
 
 	def evaluate_acc():
 		truthTable(Yhat, Y)
-		# TODO: evaluations
-		#
-		#
-
-	def selectTestsets(self):
