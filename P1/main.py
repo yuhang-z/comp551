f2a4d7fccdb180401ccf24815af2582c14f32856
serevic <37199202+serevic@users.noreply.github.com>
2020-02-09 23:29:15 -0500

    Seperated Tests for Analytical / GD for Logistic Regression

--- P1/main.py
+++ P1/main.py
@@ -3,16 +3,18 @@ from Naive_Bayes import Naive_Bayes
 
 #================ Test Logistic_Regression ==================#
 
-# lg1 = Logistic_Regression("ionosphere")
-# lg2 = Logistic_Regression("adult")
-# lg3 = Logistic_Regression("breast-cancer")
-# lg4 = Logistic_Regression("bank")
+### Analytical Solution:
 
-# ### Run .predict() for a single time test
-# # lg1.predict()
-# # lg2.predict()
-# # lg3.predict()
-# # lg4.predict()
+# lg1 = Logistic_Regression("ionosphere", 'A')
+# lg2 = Logistic_Regression("adult", 'A')
+# lg3 = Logistic_Regression("breast-cancer", 'A')
+# lg4 = Logistic_Regression("bank", 'A')
+
+### Run .predict() for a single time test
+# lg1.predict()
+# lg2.predict()
+# lg3.predict()
+# lg4.predict()
 
 # ### Run .kfoldCrossValidation() for k-fold CV accuracy score
 # lg1.kfoldCrossValidation(5)
@@ -21,21 +23,40 @@ from Naive_Bayes import Naive_Bayes
 # lg4.kfoldCrossValidation(5)
 
 
-#================ Test Naive_Bayes ==================#
+### GD Solution:
 
-lga = Naive_Bayes("ionosphere")
-# lgb = Naive_Bayes("adult")
-lgc = Naive_Bayes("breast-cancer")
-# lgd = Naive_Bayes("bank")
+lg5 = Logistic_Regression("ionosphere", 'G')
+lg6 = Logistic_Regression("adult", 'G')
+lg7 = Logistic_Regression("breast-cancer", 'G')
+lg8 = Logistic_Regression("bank", 'G')
 
-# ### Run .predict() for a single time test
-# # lga.predict()
-# # lgb.predict()
-# # lgc.predict()
-# # lgd.predict()
+### Run .predict() for a single time test
+lg5.predict()
+lg6.predict()
+lg7.predict()
+lg8.predict()
 
 ### Run .kfoldCrossValidation() for k-fold CV accuracy score
-lga.kfoldCrossValidation(5)
+lg5.kfoldCrossValidation(5)
+lg6.kfoldCrossValidation(5)
+lg7.kfoldCrossValidation(5)
+lg8.kfoldCrossValidation(5)
+
+#================ Test Naive_Bayes ==================#
+
+# lga = Naive_Bayes("ionosphere")
+# lgb = Naive_Bayes("adult")
+# lgc = Naive_Bayes("breast-cancer")
+# lgd = Naive_Bayes("bank")
+
+### Run .predict() for a single time test
+# lga.predict()
+# lgb.predict()
+# lgc.predict()
+# lgd.predict()
+
+# ### Run .kfoldCrossValidation() for k-fold CV accuracy score
+# lga.kfoldCrossValidation(5)
 # lgb.kfoldCrossValidation(5)
-lgc.kfoldCrossValidation(5)
+# lgc.kfoldCrossValidation(5)
 # lgd.kfoldCrossValidation(5)
