65277976bdd5bd16bae6e0dbb91701d83e747021
serevic <37199202+serevic@users.noreply.github.com>
2020-02-11 03:18:08 -0500

    Update main.py

--- P1/main.py
+++ P1/main.py
@@ -10,7 +10,7 @@ from Naive_Bayes import Naive_Bayes
 # lg3 = Logistic_Regression("breast-cancer", 'A')
 # lg4 = Logistic_Regression("bank", 'A')
 
-### Run .predict() for a single time test
+# Run .predict() for a single time test
 # lg1.predict()
 # lg2.predict()
 # lg3.predict()
@@ -25,22 +25,22 @@ from Naive_Bayes import Naive_Bayes
 
 ### GD Solution:
 
-lg5 = Logistic_Regression("ionosphere", 'G')
-lg6 = Logistic_Regression("adult", 'G')
+# lg5 = Logistic_Regression("ionosphere", 'G')
+# lg6 = Logistic_Regression("adult", 'G')
 lg7 = Logistic_Regression("breast-cancer", 'G')
-lg8 = Logistic_Regression("bank", 'G')
+# lg8 = Logistic_Regression("bank", 'G')
 
-### Run .predict() for a single time test
-lg5.predict()
-lg6.predict()
-lg7.predict()
-lg8.predict()
+# ### Run .predict() for a single time test
+# lg5.predict()
+# lg6.predict()
+# lg7.predict()
+# lg8.predict()
 
-### Run .kfoldCrossValidation() for k-fold CV accuracy score
-lg5.kfoldCrossValidation(5)
-lg6.kfoldCrossValidation(5)
+# ### Run .kfoldCrossValidation() for k-fold CV accuracy score
+# lg5.kfoldCrossValidation(5)
+# lg6.kfoldCrossValidation(8)
 lg7.kfoldCrossValidation(5)
-lg8.kfoldCrossValidation(5)
+# lg8.kfoldCrossValidation(8)
 
 #================ Test Naive_Bayes ==================#
 
@@ -49,13 +49,13 @@ lg8.kfoldCrossValidation(5)
 # lgc = Naive_Bayes("breast-cancer")
 # lgd = Naive_Bayes("bank")
 
-### Run .predict() for a single time test
+## Run .predict() for a single time test
 # lga.predict()
 # lgb.predict()
 # lgc.predict()
 # lgd.predict()
 
-# ### Run .kfoldCrossValidation() for k-fold CV accuracy score
+### Run .kfoldCrossValidation() for k-fold CV accuracy score
 # lga.kfoldCrossValidation(5)
 # lgb.kfoldCrossValidation(5)
 # lgc.kfoldCrossValidation(5)
