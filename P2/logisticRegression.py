e7151e25ddca44dc6ec4ecfa8529486b4388c370
Diyang Zhang <37199202+serevic@users.noreply.github.com>
2020-03-11 03:01:35 -0400

    Unsolved Issue wrt. IMDb

--- P2/logisticRegression.py
+++ P2/logisticRegression.py
@@ -34,18 +34,28 @@ LRpip = Pipeline([
 
 print("Default Parameters: solver='liblinear', which handles l1 Penalty")
 
-# ### PART I (Possible Bonus): Perform Training on training set, Predictions also on training set
+### PART I (Possible Bonus): Perform Training on training set, Predictions also on training set
 LRpip.fit(twenty_train.data, twenty_train.target)
 pred = LRpip.predict(twenty_train.data)
-print("(Bonus) Training Set Accuracy:", metrics.f1_score(twenty_train.target, pred, average='macro'))
+print("(Bonus) 20: Training Set Accuracy:", metrics.f1_score(twenty_train.target, pred, average='macro'))
 
-# ### PART II (Required): Perform Training on training set, Predictions on test set
+LRpip.fit(imdb_train.data, imdb_train.target)
+pred = LRpip.predict(imdb_train.data)
+print("(Bonus) imdb: Training Set Accuracy:", accuracy_score(twenty_train.target, pred))
+
+
+### PART II (Required): Perform Training on training set, Predictions on test set
 pred = LRpip.predict(twenty_test.data)
-print("(Required) Test Set Accuracy:", metrics.f1_score(twenty_test.target, pred, average='macro'))
+print("(Required) 20: Test Set Accuracy:", metrics.f1_score(twenty_test.target, pred, average='macro'))
+
+pred = LRpip.predict(imdb_test.data)
+print("(Required) imdb: Test Set Accuracy:", accuracy_score(twenty_test.target, pred))
 
 
 ### Part III (Required): K-Fold cross validation
-print("(Required) K-cv score before tuning:", cross_val_score(LRpip, twenty_train.data, twenty_train.target, cv=5, scoring='accuracy').mean())
+print("(Required) 20: K-cv score before tuning:", cross_val_score(LRpip, twenty_train.data, twenty_train.target, cv=5, scoring='accuracy').mean())
+
+print("(Required) imdb: K-cv score before tuning:", cross_val_score(LRpip, imdb_train.data, imdb_train.target, cv=5, scoring='accuracy').mean())
 # kf = KFold(n_splits=5, random_state=None, shuffle=False)
 # print(twenty_train.data.shape)
 # for train_index, test_index in kf.split(twenty_train):
