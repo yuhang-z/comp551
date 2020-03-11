eae65641558559d0f2fa5a74f220083bcb97883f
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2020-03-10 20:44:20 -0400

    problem with trees

--- P2/decisionTree.py
+++ P2/decisionTree.py
@@ -43,8 +43,8 @@ print("Accuracy before tuning:", results.mean())
 params = {
     "clf__max_features": ['auto', 'sqrt', 'log2'],
     "clf__criterion": ["gini", "entropy"],
-    "clf__min_samples_split": [12, 14, 16, 18, 20],
-    "clf__min_samples_leaf": [12, 14, 16, 18, 20],
+    "clf__min_samples_split": [2, 12, 14, 16, 18, 20],
+    "clf__min_samples_leaf": [1, 12, 14, 16, 18, 20],
     "clf__class_weight": ["balanced", None]
     }
 
@@ -53,9 +53,11 @@ turned_dt_random = RandomizedSearchCV(DTpip, param_distributions=params, cv=5)
 turned_dt_random.fit(twenty_train.data, twenty_train.target)
 best_estimator = turned_dt_random.best_estimator_
 
-print('Best max_depth(random search):', turned_dt_random.best_estimator_.get_params()['clf__max_depth'])
 print('Best max_features(random search):', turned_dt_random.best_estimator_.get_params()['clf__max_features'])
 print('Best criterion(random search):', turned_dt_random.best_estimator_.get_params()['clf__criterion'])
+print('Best min_samples_split(random search):', turned_dt_random.best_estimator_.get_params()['clf__min_samples_split'])
+print('Best min_samples_leaf(random search):', turned_dt_random.best_estimator_.get_params()['clf__min_samples_leaf'])
+print('Best class_weight(random search):', turned_dt_random.best_estimator_.get_params()['clf__class_weight'])
 
 y_estimated = turned_dt_random.predict(twenty_test.data)
 acc = np.mean(y_estimated == twenty_test.target)
